6c351d9a2ac043028963254b7e33fbcb49b0ab1d
Xinyue Ge <47619763+FeliciaGe@users.noreply.github.com>
2019-04-21 00:27:24 -0400

    Update HilbertExplorer.py
    Update T_step

--- flask-back/hilbert/HilbertExplorer.py
+++ flask-back/hilbert/HilbertExplorer.py
@@ -78,9 +78,9 @@ class HilbertExplorer:
         self.t = t
         self.dist = self._calDistFromT(self.t)
         
-    def T_step(self):
+    def T_step(self, cur_p):
         #self.t = self.t+(1/(2**(self.p+10)))
-        return (1/2**(self.p+5))
+        return (1/2**(cur_p+5))
     
     def _calDistFromT(self, t):
         dist = (int(t * pow(10,10)) * (2 ** (self.n * self.p) - 1)) // pow(10,10)
